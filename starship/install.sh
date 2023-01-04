c16080ca60e5671d7b941e987819e671a8af9b2b
Lucas Harrison <lharrison@personifycorp.com>
2023-01-04 15:07:23 -0600

    fixing starship

--- starship/install.sh
+++ starship/install.sh
@@ -5,4 +5,5 @@ then
         DOTFILES_LOCATION="${HOME}/dotfiles"
 fi
 
+mkdir -p $HOME/.config && touch $HOME/.config/starship.toml
 ln -sf "${DOTFILES_LOCATION}/starship/starship.toml" "${HOME}/.config/starship.toml"
