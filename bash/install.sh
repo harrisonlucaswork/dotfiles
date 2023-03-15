93556a6f775bfd05f4b294a7e3ce62c53968ebae
Lucas Harrison <lharrison@personifycorp.com>
2023-03-15 10:22:44 -0500

    updates

--- bash/install.sh
+++ bash/install.sh
@@ -9,7 +9,14 @@ ln -sf "${DOTFILES_LOCATION}/bash/bash_aliases" "${HOME}/.bash_aliases"
 ln -sf "${DOTFILES_LOCATION}/bash/bashrc" "${HOME}/.bashrc"
 ln -sf "${DOTFILES_LOCATION}/bash/bash_profile" "${HOME}/.bash_profile"
 
+# convert to zsh
+cat ${DOTFILES_LOCATION}/bash/zshrc >> generatedzsh
+cat ${DOTFILES_LOCATION}/bash/bash_aliases >> generatedzsh
+ln -sf "${DOTFILES_LOCATION}/bash/generatedzsh" "${HOME}/.zshrc"
+
+
 PATH=${DOTFILES_LOCATION}/bash:${PATH}
 source "${HOME}/.bash_profile"
 source "${HOME}/.bashrc"
 source "${HOME}/.bash_aliases"
+source "${HOME}/.zshrc"
